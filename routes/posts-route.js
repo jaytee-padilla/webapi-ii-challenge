e2c2a642fb6b24adf1a12c23db255c1e62fb0f4c
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-07 00:45:11 -0700

    added the ability to create new posts

--- routes/posts-route.js
+++ routes/posts-route.js
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
 
 	BlogData.findById(postId)
 		.then(post => {
-			if(post.length === 0){
+			if(post.length === 0) {
 				return res.status(404).json({message: "The post with the specified ID does not exist."});
 			}
 
@@ -29,6 +29,27 @@ router.get('/:id', (req, res) => {
 		})
 		.catch(error => {
 			res.status(500).json({error: "The post information could not be retrieved."});
+		});
+});
+
+// POST to /api/posts
+router.post('/', (req, res) => {
+	const postData = req.body;
+	// if title or contents is missing from the api POST request, return error
+	// otherwise, trim the whitespace from the beginning and ends of the data
+	if(!postData.title || !postData.contents) {
+		return res.status(400).json({errorMessage: "Please provide title and contents for the post."});
+	} else {
+		postData.title = postData.title.trim();
+		postData.contents = postData.contents.trim();
+	}
+
+	BlogData.insert(postData)
+		.then(post => {
+			res.status(201).json(post);
+		})
+		.catch(error => {
+			res.status(500).json({error: "There was an error while saving the post to the database"});
 		});
 });
 
